dbc49f3eb63c2310c738684778913f60bd75c755
lvhongliang <honglianglv@gmail.com>
2013-11-10 22:02:37 +0800

    add mixMarkovcpp file

---
+++ mixMarkov/mixMarkov.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void mixMarkov(vector<<vector<int> > inputMatrix, int classNum) {
+    int maxIter = 100;
+    double tol = 0.0000000000001;
+    int lineNum = inputMatrix.size();
+    int maxSeqNum = 
+
+    
+}
+
+int main(int argc, char** argv) {
+
+}
